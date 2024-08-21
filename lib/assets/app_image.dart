1f277a7c93eb307454648df487ca5794fad42752
Esther <agboe499@gmail.com>
2024-08-21 10:18:12 +0100

    Implemented password reset and Kyc two

--- lib/assets/app_image.dart
+++ lib/assets/app_image.dart
@@ -3,6 +3,9 @@ class AppImage {
   static const String logo = 'assets/images/logo.png';
   static const String closeOutlined = 'assets/images/back.png';
   static const String applogo = 'assets/images/app_logo.png';
+  static const String mail = 'assets/images/mail.png';
+  static const String security = 'assets/images/security.png';
 
-  
+
+
 }
